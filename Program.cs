b9ffd5307ba67e1f8f5da67dbe75b4a6a59bf13c
gbmatheus <gabrielmatheus2503@gmail.com>
2023-11-21 08:38:45 -0300

    chore: setting connection with sqlite

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+using TodoApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("SqliteConnectionString") ?? "Data Source=app.db;Cache=Shared";
+
+builder.Services.AddSqlite<AppDbContext>(connectionString);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
