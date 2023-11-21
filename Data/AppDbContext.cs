b550cd660350929aa7d35060de400ddc38e5f2e2
gbmatheus <gabrielmatheus2503@gmail.com>
2023-11-21 08:39:36 -0300

    feat: created todo class and mapped table

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
