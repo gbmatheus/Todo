baca0e85e0f212c02f816de36171b762afeb1a1f
gbmatheus <gabrielmatheus2503@gmail.com>
2024-08-04 22:00:19 -0300

    feat: added validation for request data into update todo use case

--- src/Api/UseCase/Todos/UpdateTodo/UpdateTodoUseCase.cs
+++ src/Api/UseCase/Todos/UpdateTodo/UpdateTodoUseCase.cs
@@ -1,28 +1,42 @@
 ﻿using Api.Comunnication.Requests.Todo;
+using Api.Domain.Enums.Todo;
 using Api.Repositories;
 
 namespace Api.UseCase.Todos.UpdateTodo
 {
     public class UpdateTodoUseCase
     {
-        public bool execute(int id, RequestUpdateTodoJson todo)
+        public bool execute(int id, RequestUpdateTodoJson request)
         {
-            var repository = new TodoDbContext();
-
-            var todoFounded = repository.Todos.FirstOrDefault(t => t.Id == id);
-
-            if (todoFounded == null) return false;
-
-            if(!string.IsNullOrWhiteSpace(todo.Name))
-                todoFounded.Name = todo.Name;
-            if (!string.IsNullOrWhiteSpace(todo.Description))
-                todoFounded.Description = todo.Description;
+            Validate(request);
             
-            if (todo.Deadline > DateTime.Today)
-                todoFounded.Deadline = todo.Deadline;
+            var repository = new TodoDbContext();
+            var todo = repository.Todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null) return false;
+
+            if(!string.IsNullOrWhiteSpace(request.Name))
+                todo.Name = request.Name;
+            if (!string.IsNullOrWhiteSpace(request.Description))
+                todo.Description = request.Description;
+            
+            if (request.Deadline > DateTime.Today)
+                todo.Deadline = (DateTime)request.Deadline;
             
             repository.SaveChanges();
             return true;
+        }
+
+        public void Validate(RequestUpdateTodoJson request)
+        {
+            if (request.Priority != null && Enum.IsDefined(typeof(Priotity), request.Priority))
+                throw new BadHttpRequestException("Priority is invalid");
+
+            if (request.Status != null && Enum.IsDefined(typeof(Status), request.Status))
+                throw new BadHttpRequestException("Status is invalid");
+
+            if (request.Deadline != null && DateTime.UtcNow > request.Deadline)
+                throw new BadHttpRequestException("Deadline cannot be in the past");
+
         }
     }
 }
