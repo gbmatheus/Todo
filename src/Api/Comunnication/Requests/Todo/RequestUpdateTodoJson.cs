cb5892426b733a252e8cb639d75c70ee9a559caf
gbmatheus <gabrielmatheus2503@gmail.com>
2024-08-04 21:58:48 -0300

    feat: changed update request data to non-mandatory

--- src/Api/Comunnication/Requests/Todo/RequestUpdateTodoJson.cs
+++ src/Api/Comunnication/Requests/Todo/RequestUpdateTodoJson.cs
@@ -4,10 +4,10 @@ namespace Api.Comunnication.Requests.Todo
 {
     public class RequestUpdateTodoJson
     {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public Priotity Priority { get; set; }
-        public Status Status { get; set; }
-        public DateTime Deadline { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public Priotity? Priority { get; set; }
+        public Status? Status { get; set; }
+        public DateTime? Deadline { get; set; }
     }
 }
